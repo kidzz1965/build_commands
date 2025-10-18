403914444db4dd53cd064c079fd4f21dd4a884cc
kidzz1965 <kidzz1965@gmail.com>
2025-10-18 09:48:02 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 rm -rf .repo/local_manifests; \
-repo init -u https://github.com/LineageOS/android.git -b lineage-23.0 --git-lfs; \
+repo init -u https://github.com/DerpFest-AOSP/android_manifest.git -b 16 --git-lfs; \
 rm -rf prebuilts/clang/host/linux-x86; \
 /opt/crave/resync.sh; \
 
