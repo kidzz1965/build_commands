242dab0a14157abf4eab1d5efb12abdceaa55a25
kidzz1965 <kidzz1965@gmail.com>
2025-10-18 09:10:18 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,37 +6,16 @@ rm -rf prebuilts/clang/host/linux-x86; \
 /opt/crave/resync.sh; \
 
 mf=(
-device/xiaomi/munch
-kernel/xiaomi/munch
-vendor/xiaomi/munch
-vendor/xiaomi/munch-firmware
+device/xiaomi/chime
+kernel/xiaomi/chime
+vendor/xiaomi/chime
 hardware/xiaomi
-hardware/dolby
-packages/resources/devicesettings
-vendor/xiaomi/miuicamera
 )
 
 rm -rf "${mf[@]}"
 
-git clone https://github.com/Lordputin404/android_device_xiaomi_munch_hdzungx -b lineage device/xiaomi/munch
-
-git clone https://github.com/Lordputin404/android_vendor_xiaomi_munch_hdzungx -b 16-exp vendor/xiaomi/munch
-
-git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware
-
-git clone https://github.com/Lordputin404/kernel_xiaomi_munch -b 16-ksu kernel/xiaomi/munch
-cd kernel/xiaomi/munch && git submodule init && git submodule update && rm -rf KernelSU-Next/userspace/su && cd ../../..; \
-
-git clone https://github.com/Lordputin404/android_hardware_xiaomi hardware/xiaomi
-
-git clone https://github.com/munch-devs/android_hardware_dolby hardware/dolby
-
-git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings
-
-#git clone https://gitlab.com/rik-x777/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
-
-git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera
+git clone https://github.com/kidzz1965/device_xiaomi_chime.git -b lineage-23.0 device/xiaomi/chime
 
 
 source build/envsetup.sh
-brunch munch
+brunch chime
