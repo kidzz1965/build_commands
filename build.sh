6d32c87fab8e9bbc7a0fe48dd71237a6ae90e0e6
kidzz1965 <kidzz1965@gmail.com>
2025-10-18 16:42:02 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ hardware/xiaomi
 rm -rf "${mf[@]}"
 
 git clone https://github.com/kidzz1965/device_xiaomi_chime.git -b lineage-23.0 device/xiaomi/chime
-
+git clone https://github.com/Evolution-X-Devices/hardware_dolby.git -b bka-aospa hardware/dolby
 
 source build/envsetup.sh
 brunch chime
