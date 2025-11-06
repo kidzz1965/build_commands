5b7c8eb31641d7e3ea38672bcdc4a147d9c09142
kidzz1965 <kidzz1965@gmail.com>
2025-11-06 14:55:10 +0700

    lol

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 rm -rf .repo/local_manifests; \
-repo init -u https://github.com/WitAqua/manifest.git -b 16.0 --git-lfs; \
+repo init -u https://github.com/LumineDroid/platform_manifest.git -b bynx --git-lfs; \
 rm -rf prebuilts/clang/host/linux-x86; \
 /opt/crave/resync.sh; \
 
@@ -19,11 +19,10 @@ rm -rf "${mf[@]}"
 git clone https://github.com/kidzz1965/device_xiaomi_chime.git -b lineage-23.0 device/xiaomi/chime
 
 source build/envsetup.sh
-export WITAQUA_BUILD_TYPE="UNOFFICIAL"
-export WITAQUA_MAINTAINER="Kidzz"
+export LUMINE_MAINTAINER="Kidzz"
 lunch lineage_chime-bp2a-userdebug
 mka bacon
 
 wget https://raw.githubusercontent.com/Sushrut1101/GoFile-Upload/refs/heads/master/upload.sh
 chmod +x upload.sh
-./upload.sh out/target/product/chime/WitAqua*chime*.zip
+./upload.sh out/target/product/chime/LumineDroid*chime*.zip
